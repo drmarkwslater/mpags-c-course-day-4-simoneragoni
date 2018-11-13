c75d0ea43d8e6e052bb09fe625fc5340c46ccbe8
Simone Ragoni <simone.ragoni.94@gmail.com>
2018-11-13 11:29:45 +0100

    Completed DESIGN of 4th day assignment: seems to be working with a trial text file too!

--- MPAGSCipher/PlayfairCipher.cpp
+++ MPAGSCipher/PlayfairCipher.cpp
@@ -16,7 +16,7 @@
 
 //_______________________________________________________
 PlayfairCipher::PlayfairCipher( const std::string& key )
-  : key_{0}
+  //: key_{0}
 {
   // We have the key as a string, but the cipher needs a 5x5 grid, so we first need to convert it.
   // We default to having a key of 0, i.e. no encryption, if no (valid) key was provided on the command line
@@ -36,8 +36,34 @@ void PlayfairCipher::setKey( const std::string& key )
  * - Copying the full alphabet just by adding it
  * - to the end iterator of the original vector.
  */
+  std::copy( alphabet_.begin(), alphabet_.end(), back_inserter(key_) );
+  // key_.push_back('A');
+  // key_.push_back('B');
+  // key_.push_back('C');
+  // key_.push_back('D');
+  // key_.push_back('E');
+  // key_.push_back('F');
+  // key_.push_back('G');
+  // key_.push_back('H');
+  // key_.push_back('I');
+  // key_.push_back('J');
+  // key_.push_back('K');
+  // key_.push_back('L');
+  // key_.push_back('M');
+  // key_.push_back('N');
+  // key_.push_back('O');
+  // key_.push_back('P');
+  // key_.push_back('Q');
+  // key_.push_back('R');
+  // key_.push_back('S');
+  // key_.push_back('T');
+  // key_.push_back('U');
+  // key_.push_back('V');
+  // key_.push_back('W');
+  // key_.push_back('X');
+  // key_.push_back('Y');
+  // key_.push_back('Z');
 
-  std::copy( alphabet_.begin(), alphabet_.end(), key_.end() );
 
   // Make sure the key is upper case
 
@@ -48,7 +74,6 @@ void PlayfairCipher::setKey( const std::string& key )
    * - A slightly different variant is commented just
    * - below the official version. NOT TESTED.
    */
-
   std::transform( key_.begin(), key_.end(), key_.begin() , ::toupper );
   // std::transform( key_.begin(), key_.end(), key_.begin() , [](char &c){ c = ::toupper(c); } );
 
@@ -61,7 +86,6 @@ void PlayfairCipher::setKey( const std::string& key )
    * - The remove function returns an iterator to the beginning of
    * - the postponed character, thus enabling the ACTUAL ERASING act.
    */
-
   key_.erase(std::remove_if(key_.begin(), key_.end(),
              [](char elem){return !isalpha(elem);}  ), key_.end() );
 
@@ -72,7 +96,6 @@ void PlayfairCipher::setKey( const std::string& key )
    * - by usage of LAMBDA CHECK function in conjunction
    * - to std::transform.
    */
-
   std::transform(key_.begin(), key_.end(), key_.begin(),
                  [](char &c){ if ( c == 'J') return 'I'; else return c; } );
 
@@ -87,7 +110,6 @@ void PlayfairCipher::setKey( const std::string& key )
  *  - for later checks.
  * - This action HAS to happen AFTER changing all Js to Is.
  */
-
   std::string seen;
   key_.erase(std::remove_if(key_.begin(), key_.end(),
              [&seen](const char &value)
@@ -103,14 +125,12 @@ void PlayfairCipher::setKey( const std::string& key )
  * - Created a cartesian grid of size_t with the expected XY coords.
  *
  */
-
  Cartesian fiveByFiveGrid[5][5];
  for (size_t i = 0; i < 5; i++) {
       for (size_t j = 0; j < 5; j++) {
             fiveByFiveGrid[i][j] = std::make_pair(i,j);
       }
  }
-
   // Store the playfair cipher key map
 
   /* - SIMONE IN:
@@ -120,14 +140,13 @@ void PlayfairCipher::setKey( const std::string& key )
    * - carry-on from the grid limitations.
    */
 
- std::cout << key_;
+ std::cout << key_ << std::endl;
  for (size_t i = 0; i < 5; i++) {
       for (size_t j = 0; j < 5; j++) {
             atlas_.insert(std::make_pair( key_.at( i*5 + j ), fiveByFiveGrid[i][j] ));
             hasbro_.insert(std::make_pair( fiveByFiveGrid[i][j], key_.at( i*5 + j ) ));
       }
  }
-
 }
 
 
@@ -144,7 +163,6 @@ std::string PlayfairCipher::applyCipher( const std::string& inputText,
    * - by usage of LAMBDA CHECK function in conjunction
    * - to std::transform.
    */
-
   std::transform(outputText.begin(), outputText.end(), outputText.begin(),
                  [](char &c){ if ( c == 'J') return 'I'; else return c; } );
 
@@ -164,14 +182,16 @@ std::string PlayfairCipher::applyCipher( const std::string& inputText,
  *  - so that the logic does not get affected but the inner order does.
  *  - This action HAS to happen AFTER changing all Js to Is.
  */
-
   size_t counter = 0;
-  for ( auto iter { std::begin(outputText) }; iter != outputText.end(); ++iter )
+  size_t duplica = 0;
+  for ( auto iter { std::begin(outputText) }; iter !=  outputText.end() + duplica - 2; ++iter )
   {
-        if( counter%2 == 0 && *iter == *(iter + 1) )
+        if( ( counter%2 == 0 ) &&  (*iter == *(iter + 1) ) )
         {
             if ( *iter == 'X' )  outputText.insert(iter + 1, 'Q');
             else                 outputText.insert(iter + 1, 'X');
+            ++duplica;
+
         }
         ++counter;
   }
@@ -189,7 +209,6 @@ std::string PlayfairCipher::applyCipher( const std::string& inputText,
   // - Find the coords in the grid for each digraph
   // - Apply the rules to these coords to get 'new' coords
   // - Find the letter associated with the new coords
-
 
 if(cipherMode == CipherMode::Encrypt)
 {
